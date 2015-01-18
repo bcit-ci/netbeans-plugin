904e9772191ce3d0a18bd9ec51b3cf3195472c78
denev2004 <denev2004@localhost>
2015-01-18 11:21:36 -0800

    Proposed Simplified Chinese translation to address #78 Signed-off-by:denev2004 <denev2004@localhost>

---
+++ src/com/codeigniter/netbeans/navigator/CiHyperlinkProviderBase.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codeigniter.netbeans.navigator;
+
+import org.netbeans.lib.editor.hyperlink.spi.HyperlinkProviderExt;
+
+/**
+ *
+ * @author Tamaki_Sakura
+ */
+public abstract class CiHyperlinkProviderBase implements HyperlinkProviderExt {
+    
+}
