b30c76144fd09eff8f1776094c70ef2440dd7540
Heesung Ahn <ahn.heesung@gmail.com>
2015-02-04 12:16:33 -0500

    Initial version Signed-off-by:Heesung Ahn <ahn.heesung@gmail.com>

---
+++ src/com/codeigniter/netbeans/manager/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@OptionsPanelController.ContainerRegistration(id = "CodeIgniter", categoryName = "#OptionsCategory_Name_CodeIgniter", iconBase = "com/codeigniter/netbeans/generator/cilogo32.png", keywords = "#OptionsCategory_Keywords_CodeIgniter", keywordsCategory = "CodeIgniter")
+@NbBundle.Messages(value = {"OptionsCategory_Name_CodeIgniter=CodeIgniter", "OptionsCategory_Keywords_CodeIgniter=CodeIgniter"})
+package com.codeigniter.netbeans.manager;
+
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.NbBundle;
