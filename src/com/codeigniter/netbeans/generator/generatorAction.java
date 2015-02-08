5be335d66d6a45637de22f495a4d557d46d44d0c
Maxence Coulibaly <madmax330@yahoo.com>
2015-02-07 20:58:49 -0500

    Added generator wizard on CI button click, and in the tools menu

--- src/com/codeigniter/netbeans/generator/generatorAction.java
+++ src/com/codeigniter/netbeans/generator/generatorAction.java
@@ -5,8 +5,15 @@
  */
 package com.codeigniter.netbeans.generator;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JComponent;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
@@ -18,7 +25,7 @@ import org.openide.util.NbBundle.Messages;
         id = "com.codeigniter.netbeans.generator.generatorAction"
 )
 @ActionRegistration(
-        iconBase = "com/codeigniter/netbeans/generator/logo16.png",
+        iconBase = "com/codeigniter/netbeans/generator/cilogo16.png",
         displayName = "#CTL_generatorAction"
 )
 @ActionReferences({
@@ -32,5 +39,29 @@ public final class generatorAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO implement action body
+        List<WizardDescriptor.Panel<WizardDescriptor>> panels = new ArrayList<WizardDescriptor.Panel<WizardDescriptor>>();
+        panels.add(new GenwizardWizardPanel1());
+        panels.add(new GenwizardWizardPanel2());
+        String[] steps = new String[panels.size()];
+        for (int i = 0; i < panels.size(); i++) {
+            Component c = panels.get(i).getComponent();
+            // Default step name to component name of panel.
+            steps[i] = c.getName();
+            if (c instanceof JComponent) { // assume Swing components
+                JComponent jc = (JComponent) c;
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX, i);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA, steps);
+                jc.putClientProperty(WizardDescriptor.PROP_AUTO_WIZARD_STYLE, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_DISPLAYED, true);
+                jc.putClientProperty(WizardDescriptor.PROP_CONTENT_NUMBERED, true);
+            }
+        }
+        WizardDescriptor wiz = new WizardDescriptor(new WizardDescriptor.ArrayIterator<WizardDescriptor>(panels));
+        // {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
+        wiz.setTitleFormat(new MessageFormat("{0}"));
+        wiz.setTitle("Generator");
+        if (DialogDisplayer.getDefault().notify(wiz) == WizardDescriptor.FINISH_OPTION) {
+            // do something
+        }
     }
 }
