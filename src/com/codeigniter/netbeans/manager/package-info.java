935aff0fd8a01282e7243f01d70067240c901b91
Heesung Ahn <ahn.heesung@gmail.com>
2015-02-09 14:10:27 -0500

    Updated Path for Logo using shared assets. Signed-off-by:Heesung Ahn <ahn.heesung@gmail.com>

--- src/com/codeigniter/netbeans/manager/package-info.java
+++ src/com/codeigniter/netbeans/manager/package-info.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */// Position default is 2147483647
-@OptionsPanelController.ContainerRegistration(id = "CodeIgniter", categoryName = "#OptionsCategory_Name_CodeIgniter", iconBase = "com/codeigniter/netbeans/generator/cilogo32.png", keywords = "#OptionsCategory_Keywords_CodeIgniter", keywordsCategory = "CodeIgniter", position = 200000000)
+@OptionsPanelController.ContainerRegistration(id = "CodeIgniter", categoryName = "#OptionsCategory_Name_CodeIgniter", iconBase = "com/codeigniter/netbeans/shared/Assets/cilogo32.png", keywords = "#OptionsCategory_Keywords_CodeIgniter", keywordsCategory = "CodeIgniter", position = 200000000)
 @NbBundle.Messages(value = {"OptionsCategory_Name_CodeIgniter=CodeIgniter", "OptionsCategory_Keywords_CodeIgniter=CodeIgniter"})
 package com.codeigniter.netbeans.manager;
 
